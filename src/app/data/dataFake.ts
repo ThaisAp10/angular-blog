8062afd62ef4fe8b337f2ba6ddc88dee7e40b7e9
ThaisAp10 <thaisapare.10@gmail.com>
2023-05-24 10:17:47 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,50 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "ARENA MRV",
+        "description": "Futura casa do Clube Atlético Mineiro (Galo) será a mais moderna e tecnológica da América Latina.",
+        "photoCover": "https://bhaz.com.br/wp-content/uploads/2019/05/arena-mrv.jpg"
+    },
+    {
+        "id": "2",
+        "title": "GALO PRÓXIMO DE FECHAR COM SAF",
+        "description": "Americano Peter Grieve tem interesse na SAF do Galo. As partes interessadas estão em conversa.",
+        "photoCover": "https://lncimg.lance.com.br/cdn-cgi/image/width=3840,height=undefined,quality=75/uploads/2023/05/image_placeholder-1-aspect-ratio-512-320-1-95.jpg"
+    },
+    {
+        "id": "3",
+        "title": "EVENTO DE INAUGURAÇÃO DA ARENA MRV",
+        "description": "Evento ocorreu no dia 15/04/2023 e teve chutes ao gol, atrações musicais e muita emoção para o torcedor atleticano.",
+        "photoCover": "https://conteudo.imguol.com.br/c/esporte/aa/2023/04/15/galo-bastiao-no-gramado-da-arena-mrv-estadio-do-atletico-mg-1681584030068_v2_1920x1278.jpg"
+    },
+    {
+        "id": "4",
+        "title": "'LENDAS DO GALO', EVENTO TERÁ PAGODE",
+        "description": "Evento 'Lendas do Galo' com previsão de ocorrer no dia 16 de julho, terá show do cantor Péricles.",
+        "photoCover": "https://www.otempo.com.br/image/contentid/policy:1.2867911:1683903000/Pericles-jfif.jfif?f=3x2&w=1224"
+    },
+    {
+        "id": "5",
+        "title": "O FOCO DAS VINGADORAS",
+        "description": "A equipe de futebol feminino do Clube Atlético Mineiro continuam com foco total no campeonato brasileiro feminino. Na 10ª rodada pelo brasileirão feminino, as vingadoras seguem com 12 pontos na tabela de classificação, em 11º lugar.",
+        "photoCover": "https://atletico.com.br/wp-content/uploads/2023/05/foto-scaled.jpg"
+    },
+    {
+        "id": "6",
+        "title": "Galo com campanha de conscientizacao-sobre o autismo",
+        "description": "Em uma partida do campeonato brasileiro, o time mineiro trouxe na camisa, números, letras e patch alusivos ao mês de Conscientização do Autismo.",
+        "photoCover": "https://atletico.com.br/wp-content/uploads/2023/04/4-1.png"
+    },
+    {
+        "id": "7",
+        "title": "Galo na veia",
+        "description": "O Galo na Veia, programa de sócios do Atlético-MG, já chegou ao número de 130 mil planos ativos. Porém em 2022 houve uma queda.",
+        "photoCover": "https://galonaveia.atletico.com.br/assets/imgs/logos/galo.png"
+    },
+    {
+        "id": "8",
+        "title": "Galo TV",
+        "description": "Galotv transmitirá alguns jogos das Vingadoras.",
+        "photoCover": "https://yt3.googleusercontent.com/UMNGTPhBYuewQ-PM-WTm2v6EgP3KLnn2xRGOeQ2TyBFY8tLzHr0wyUlRdC9PJYUGtTwAXAYw7-U=s900-c-k-c0x00ffffff-no-rj"
+    }
+]
